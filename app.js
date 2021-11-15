2a4b0d7b701f98552d598da82696385e345f4dee
Jeremy Tdr <jeremy.tdr@gmail.com>
2021-11-15 19:11:29 +0100

    Random number between 1 and 6 added + dice img

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ let currentPlayer2 = document.querySelector('.player2-current .current-score');
 const rollDice = document.getElementById('roll-dice');
 const holdDice = document.getElementById('hold-dice');
 
+let images = ["dice-1.svg","dice-2.svg","dice-3.svg","dice-4.svg","dice-5.svg","dice-6.svg"]
+
+let diceImg = document.querySelector('.dice-img')
+
+currentP1 = 0
 
 globalPlayer1.innerHTML = 0
 globalPlayer2.innerHTML = 0
-currentPlayer1.innerHTML = 0
+currentPlayer1.innerHTML = currentP1
 currentPlayer2.innerHTML = 0
 
 newGame.addEventListener('click', () => {
     globalPlayer1.innerHTML = 0
     globalPlayer2.innerHTML = 0
-    currentPlayer1.innerHTML = 0
+    currentP1 = 0
     currentPlayer2.innerHTML = 0
 
     player.classList.remove('active')
@@ -25,3 +30,20 @@ newGame.addEventListener('click', () => {
 
     
 })
+
+const playDice = () => {
+    rollDice.addEventListener('click', () => {
+        let diceValue = Math.floor(Math.random() * 6) + 1
+        diceImg.src=`./images/dice-${diceValue}.svg`;
+        if (diceValue === 1){
+            currentP1 = 0
+        } else {
+            currentP1 += diceValue
+        }
+        currentPlayer1.innerHTML = currentP1
+    })
+}
+
+playDice()
+
+
