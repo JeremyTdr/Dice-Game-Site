8538c07eb86b12160b17ae69ce0360a67a09d2bd
Jeremy Tdr <jeremy.tdr@gmail.com>
2021-11-17 00:48:36 +0100

    Trying to use JS algo with id 0 & 1 (seems ok)

--- app.js
+++ app.js
@@ -1,65 +1,113 @@
-/*--- Variables pour les différents éléments du plateau de jeu ---*/
-const newGame = document.getElementById('new_game');
-const player = document.querySelectorAll('.player');
-let globalPlayers = document.querySelectorAll('.player-score');
-let dice = document.querySelector('.dice-img');
-let currentPlayers = document.querySelectorAll('.current-score');
-const rollDice = document.getElementById('roll-dice');
-const holdDice = document.getElementById('hold-dice');
+window.onload = () => {
 
-/*--- Tableau et variable des images du Dé ---*/
-let images = ["dice-1.svg","dice-2.svg","dice-3.svg","dice-4.svg","dice-5.svg","dice-6.svg"]
-let diceImg = document.querySelector('.dice-img')
+    /*--- Variables pour les différents éléments du plateau de jeu ---*/
+    const newGame = document.getElementById('new_game');
+    const player = document.querySelectorAll('.player');
+    let globalPlayers = document.querySelectorAll('.player-score');
+    let dice = document.querySelector('.dice-img');
+    let currentPlayers = document.querySelectorAll('.current-score');
+    const rollDice = document.getElementById('roll-dice');
+    const holdDice = document.getElementById('hold-dice');
 
-/*--- Variables supplémentaires pour les algos ---*/
-let globalScore, currentScore
+    /*--- Tableau et variable des images du Dé ---*/
+    let images = ["dice-1.svg","dice-2.svg","dice-3.svg","dice-4.svg","dice-5.svg","dice-6.svg"]
+    let diceImg = document.querySelector('.dice-img')
+
+    /*--- Variables supplémentaires pour les algos ---*/
+    let globalScoreP1 = 0
+    let globalScoreP2 = 0
+    let currentScore = 0
+    let actualPlayer = 0
 
 
 
+    /*--- Fonction New Game ---*/
+    const playNewGame = () => {
+        newGame.addEventListener('click', () => {
+            let globalScoreP1 = 0
+            let globalScoreP2 = 0
+            let currentScore = 0
+            let actualPlayer = 0
 
-/*--- Fonction New Game ---*/
-const playNewGame = () => {
-    newGame.addEventListener('click', () => {
-        globalPlayers.forEach(globalPlayer => {
-            globalPlayer.textContent = 0
+            globalPlayers.forEach(globalPlayer => {
+                globalPlayer.textContent = 0
+            })
+            currentPlayers.forEach(currentPlayer => {
+                currentPlayer.textContent = 0
+            })
+
+            diceImg.src = './images/dice-1.svg'
         })
-        currentPlayers.forEach(currentPlayer => {
-            currentPlayer.textContent = 0
+    }
+
+
+    playDice = () => {
+        rollDice.addEventListener('click', () => {
+            let diceValue = Math.floor(Math.random() * 6) + 1
+            diceImg.src=`./images/dice-${diceValue}.svg`
+
+            if (diceValue === 1){
+                currentScore = 0
+                if (actualPlayer == 0) {
+                    currentPlayers[0].textContent = currentScore
+                    actualPlayer = 1
+                } else {
+                    currentPlayers[1].textContent = currentScore
+                    actualPlayer = 0
+                }
+            } else {
+                currentScore += diceValue
+                currentPlayers[actualPlayer].textContent = currentScore
+            }    
         })
-        diceImg.src = './images/dice-1.svg'
-    })
-}
+    }
 
-/*--- Fonction Dé random ---*/
-playDice = () => {
-    rollDice.addEventListener('click', () => {
-        let diceValue = Math.floor(Math.random() * 6) + 1
-        diceImg.src=`./images/dice-${diceValue}.svg`
+    /*--- Fonction Dé random 
+    playDice = () => {
+        rollDice.addEventListener('click', () => {
+            let diceValue = Math.floor(Math.random() * 6) + 1
+            diceImg.src=`./images/dice-${diceValue}.svg`
 
-        if (diceValue === 1){
-            currentScore = 0
-        } else {
-            currentScore += diceValue
-        }
-        currentPlayers.forEach(currentPlayer => {
-            if (currentPlayer.classList.contains('active')) {
-                currentPlayer.textContent = currentScore
-            } 
+            if (diceValue === 1){
+                currentScore = 0
+            } else {
+                currentScore += diceValue
+            }
+            currentPlayers.forEach(currentPlayer => {
+                if (currentPlayer.classList.contains('active')) {
+                    currentPlayer.textContent = currentScore
+                } 
+            })
         })
-    })
-}
+    }
+    ---*/
 
-/*
-const holdGame = () => {
-    holdDice.addEventListener('click', () => {
-        globalPlayer += currentPlayer
-        globalPText.textContent = globalPlayer
-    })
-}
-*/
+    
+    const holdGame = () => {
+        holdDice.addEventListener('click', () => {
+                if (actualPlayer == 0) {
+                    globalScoreP1 += currentScore
+                    globalPlayers[0].textContent = globalScoreP1
+                    currentScore = 0
+                    currentPlayers[0].textContent = currentScore
+                    actualPlayer = 1
+                } else {
+                    globalScoreP2 += currentScore
+                    globalPlayers[1].textContent = globalScoreP2
+                    currentScore = 0
+                    currentPlayers[1].textContent = currentScore
+                    actualPlayer = 0
+                }
+        })
+    }
 
-playNewGame()
-playDice()
+
+
+    playDice()
+    holdGame()
+    playNewGame()
+
+}
 
 
 
