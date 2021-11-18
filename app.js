42cd89284276839e2707a4f9d80cab05c537ec96
Jeremy Tdr <jeremy.tdr@gmail.com>
2021-11-18 19:00:58 +0100

    Dice rolling animation added

--- app.js
+++ app.js
@@ -1,127 +1,126 @@
 window.onload = () => {
+  /*--- Variables pour les différents éléments du plateau de jeu ---*/
+  const newGame = document.getElementById("new_game");
+  const player = document.querySelectorAll(".player");
+  let globalPlayers = document.querySelectorAll(".player-score");
+  let dice = document.querySelector(".dice-img");
+  let currentPlayers = document.querySelectorAll(".current-score");
+  const rollDice = document.getElementById("roll-dice");
+  const holdDice = document.getElementById("hold-dice");
+  const mainBoard = document.querySelector(".main-board");
 
-    /*--- Variables pour les différents éléments du plateau de jeu ---*/
-    const newGame = document.getElementById('new_game');
-    const player = document.querySelectorAll('.player');
-    let globalPlayers = document.querySelectorAll('.player-score');
-    let dice = document.querySelector('.dice-img');
-    let currentPlayers = document.querySelectorAll('.current-score');
-    const rollDice = document.getElementById('roll-dice');
-    const holdDice = document.getElementById('hold-dice');
-    const mainBoard = document.querySelector('.main-board')
+  /*--- Tableau et variable des images du Dé ---*/
+  let images = [
+    "dice-1.svg",
+    "dice-2.svg",
+    "dice-3.svg",
+    "dice-4.svg",
+    "dice-5.svg",
+    "dice-6.svg",
+  ];
+  let diceImg = document.querySelector(".dice-img");
 
-    /*--- Tableau et variable des images du Dé ---*/
-    let images = ["dice-1.svg","dice-2.svg","dice-3.svg","dice-4.svg","dice-5.svg","dice-6.svg"]
-    let diceImg = document.querySelector('.dice-img')
+  /*--- Variables supplémentaires pour les algos ---*/
+  let globalScoreP1 = 0;
+  let globalScoreP2 = 0;
+  let currentScore = 0;
+  let actualPlayer = 0;
 
-    /*--- Variables supplémentaires pour les algos ---*/
-    let globalScoreP1 = 0
-    let globalScoreP2 = 0
-    let currentScore = 0
-    let actualPlayer = 0
+  /*--- Fonction Play ---*/
+  const playGame = () => {
+    initGame();
+    playDice();
+    holdGame();
+    playNewGame();
+  };
 
+  /*--- Fonction Initialization ---*/
+  const initGame = () => {
+    globalScoreP1 = 0;
+    globalScoreP2 = 0;
+    currentScore = 0;
+    actualPlayer = 0;
 
+    globalPlayers.forEach((globalPlayer) => {
+      globalPlayer.textContent = 0;
+    });
+    currentPlayers.forEach((currentPlayer) => {
+      currentPlayer.textContent = 0;
+    });
 
-    /*--- Fonction Play ---*/
-    const playGame = () => {
-        initGame()
-        playDice()
-        holdGame()
-        playNewGame()
-    }
+    switchToPlayer1();
 
+    diceImg.src = "./images/dice-6.svg";
+  };
 
-    /*--- Fonction Initialization ---*/
-    const initGame = () => {
-        globalScoreP1 = 0
-        globalScoreP2 = 0
-        currentScore = 0
-        actualPlayer = 0
+  /*--- Fonction Roll dice ---*/
+  const playDice = () => {
+    rollDice.addEventListener("click", () => {
+      diceImg.classList.add("animate");
+      setTimeout(() => {
+        diceImg.classList.remove("animate");
+      }, 500);
+      let diceValue = Math.floor(Math.random() * 6) + 1;
+      diceImg.src = `./images/dice-${diceValue}.svg`;
 
-        globalPlayers.forEach(globalPlayer => {
-            globalPlayer.textContent = 0
-        })
-        currentPlayers.forEach(currentPlayer => {
-            currentPlayer.textContent = 0
-        })
+      if (diceValue === 1) {
+        currentScore = 0;
+        if (actualPlayer == 0) {
+          currentPlayers[0].textContent = currentScore;
+          switchToPlayer2();
+        } else {
+          currentPlayers[1].textContent = currentScore;
+          switchToPlayer1();
+        }
+      } else {
+        currentScore += diceValue;
+        currentPlayers[actualPlayer].textContent = currentScore;
+      }
+    });
+  };
 
-        switchToPlayer1()
+  /*--- Fonction Hold button ---*/
+  const holdGame = () => {
+    holdDice.addEventListener("click", () => {
+      if (actualPlayer == 0) {
+        globalScoreP1 += currentScore;
+        globalPlayers[0].textContent = globalScoreP1;
+        currentScore = 0;
+        currentPlayers[0].textContent = currentScore;
+        switchToPlayer2();
+      } else {
+        globalScoreP2 += currentScore;
+        globalPlayers[1].textContent = globalScoreP2;
+        currentScore = 0;
+        currentPlayers[1].textContent = currentScore;
+        switchToPlayer1();
+      }
+    });
+  };
 
-        diceImg.src = './images/dice-6.svg'
-    }
+  /*--- Fonction New Game ---*/
+  const playNewGame = () => {
+    newGame.addEventListener("click", () => {
+      initGame();
+    });
+  };
 
-    /*--- Fonction Roll dice ---*/
-    const playDice = () => {
-        rollDice.addEventListener('click', () => {
-            let diceValue = Math.floor(Math.random() * 6) + 1
-            diceImg.src=`./images/dice-${diceValue}.svg`
+  /*--- Fonction Switch Players ---*/
+  const switchToPlayer1 = () => {
+    player[1].classList.remove("active");
+    player[0].classList.add("active");
+    mainBoard.style.background =
+      "linear-gradient(90deg, #cccccc 50%, #ffffff 50%)";
+    actualPlayer = 0;
+  };
 
-            if (diceValue === 1){
-                currentScore = 0
-                if (actualPlayer == 0) {
-                    currentPlayers[0].textContent = currentScore
-                    switchToPlayer2()
-                } else {
-                    currentPlayers[1].textContent = currentScore
-                    switchToPlayer1()
-                }
-            } else {
-                currentScore += diceValue
-                currentPlayers[actualPlayer].textContent = currentScore
-            }    
-        })
-    }
+  const switchToPlayer2 = () => {
+    player[0].classList.remove("active");
+    player[1].classList.add("active");
+    mainBoard.style.background =
+      "linear-gradient(270deg, #cccccc 50%, #ffffff 50%)";
+    actualPlayer = 1;
+  };
 
-    /*--- Fonction Hold button ---*/
-    const holdGame = () => {
-        holdDice.addEventListener('click', () => {
-                if (actualPlayer == 0) {
-                    globalScoreP1 += currentScore
-                    globalPlayers[0].textContent = globalScoreP1
-                    currentScore = 0
-                    currentPlayers[0].textContent = currentScore
-                    switchToPlayer2()
-
-                } else {
-                    globalScoreP2 += currentScore
-                    globalPlayers[1].textContent = globalScoreP2
-                    currentScore = 0
-                    currentPlayers[1].textContent = currentScore
-                    switchToPlayer1()
-                }
-        })
-    }
-
-    /*--- Fonction New Game ---*/
-    const playNewGame = () => {
-        newGame.addEventListener('click', () => {
-            initGame()
-        })   
-    }
-
-    /*--- Fonction Switch Players ---*/
-    const switchToPlayer1 = () => {
-        player[1].classList.remove('active')
-        player[0].classList.add('active')
-        mainBoard.style.background = 'linear-gradient(90deg, #cccccc 50%, #ffffff 50%)'
-        actualPlayer = 0
-    }
-
-    const switchToPlayer2 = () => {
-        player[0].classList.remove('active')
-        player[1].classList.add('active')
-        mainBoard.style.background = 'linear-gradient(270deg, #cccccc 50%, #ffffff 50%)'
-        actualPlayer = 1
-    }
-
-
-    playGame()
-
-}
-
-
-
-
-
-
-
+  playGame();
+};
